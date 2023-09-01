ce09f0f33893ccfc7140445432ed67ab959c3acf
Ishikajain02 <ishijain02@gmail.com>
2023-09-01 19:19:50 +0530

    Menu_list updated using useparams +API

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useState , useEffect } from "react";
-import url from "../utils/constant";
+import {MENU_API} from "../utils/constant";
+import Shimmer from "./Shimmer";
 const RestaurantMenu =()=>{
  const {id } = useParams();
- const [restaurant , setRestaurant] =useState({});
+ const [resInfo , setresInfo] =useState(null);
   //  const id = params.id;
    // const name = params.name;
     //const areaName = params.areaName;
@@ -16,10 +17,22 @@ const RestaurantMenu =()=>{
     getRestaurants();
     },[])
   async function getRestaurants(){
-        const data = await fetch(url);
+        const data = await fetch(MENU_API + id);
         const data2 = await data.json();
-        setRestaurant(data2);
+        const {name} =data2?.data?.cards[0]?.card?.card?.info;
+        console.log(name);
+        setresInfo(data2);
     }
+    if(resInfo== null)return <Shimmer/>
+ //   const {name,cuisines,costForTwoMessage} = resInfo?.cards[2]?.card?.card?.info;
+   const {name,cuisines ,costForTwoMessage} = resInfo?.data?.cards[0]?.card?.card?.info;
+   console.log(name);
+   //console.log(name);
+    const{itemCards} = resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  //const{itemCards} = resInfo?.cards[1]?.card?.card?.info;
+    console.log(itemCards);
+   //  console.log(name);
+
     return(
         <>
         
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,3 @@
 export const url ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83667";
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.083909325810092&lng=77.64086888929793&restaurantId=";
