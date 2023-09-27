a916762df58d2a33b7d6373488c074a97d95cccc
Ishikajain02 <ishijain02@gmail.com>
2023-09-27 19:23:30 +0530

    testing_done

---
+++ src/components/Sum.js
@@ -0,0 +1,5 @@
+//function Sum(a, b) {
+  //  return a + b;
+  //}
+ // module.exports = Sum;
+ export const Sum = (a,b) => a+b;
---
+++ src/components/__tests__/sum.test.js
@@ -0,0 +1,5 @@
+import {Sum} from "../Sum"
+
+test("check for positive number",()=>{
+  expect(Sum(2,5)).toBe(7);
+});
