74cb0c4e7758a0cc1f15b412a91e2879b7ebad31
Ishikajain02 <ishijain02@gmail.com>
2023-12-17 03:51:07 +0530

    commit

---
+++ dist/Trying.12c35ca7.js
@@ -0,0 +1,2 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire8dbe;e.register("fdEMw",function(d,n){var o,i=e("ayMG0");Object.defineProperty(d.exports,"__esModule",{value:!0}),d.exports.default=function(){return(0,i.jsx)("div",{children:"Trying"})},(o=e("acw62"))&&o.__esModule});
+//# sourceMappingURL=Trying.12c35ca7.js.map
