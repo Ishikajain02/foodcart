816449ac8cf493e6e5f445b39e9c8d0453d385ee
Ishikajain02 <ishijain02@gmail.com>
2024-04-05 21:58:26 +0530

    Cart button added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,16 @@
-import { UseSelector, useSelector } from "react-redux";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
 import FoodItem from './FoodItem';
+import { clearCart } from "../utils/cartSlice";
 const Cart = () =>{
     const carditems = useSelector((store)=> store.cart.items);
    // console.log(carditems);
+   const dispatch=useDispatch();
     return(
         <>
         <h1 className="text-3xl font-bold">Cart</h1>
-       {carditems.map(item =><FoodItem key={item.item.card.info.id} {...item.item.card.info}/>) }
+       {carditems.map(item =><FoodItem key={item.item.card.info.id} {...item.item.card.info}{...item}/>) }
       {/* <FoodItem {...carditems[0].item.card.info}/>*/}
-      <button className="p-2 m-2 bg-red-400">Clear Cart</button>
+      <button className="p-2 m-2 bg-red-400" onClick={()=>dispatch(clearCart())}>Clear Cart</button>
         </>
     );
 };
--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,4 +1,8 @@
 //import { UseSelector, useSelector } from "react-redux";
+import { addItem, clearCart, removeItem } from "../utils/cartSlice";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus ,faMinus } from '@fortawesome/free-solid-svg-icons'
+import { useDispatch } from "react-redux";
 const FoodItem = ({
     
     name,
@@ -6,20 +10,28 @@ const FoodItem = ({
     areaName,
     imageId,
     price,
-    description
+    description,item
 
 }) =>{
+    const dispatch=useDispatch();
     return(
         <>
         
       
         <div className="card ">
-        <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" + imageId } />
+        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + imageId } />
          <h2>{name}</h2>
          <h4>{cuisines}</h4>
          <h4>{areaName}</h4>
          <p>{description}</p>
          <h4>{price/100}</h4>
+         <div className="flex">
+         <button className="p-1 m-1 " onClick={()=>{
+                dispatch(addItem({item}))
+               }}>  <FontAwesomeIcon icon={faPlus}/></button>   <button className=" p-1 m-1"onClick ={()=>{
+                dispatch(removeItem());
+               }}> <FontAwesomeIcon icon={faMinus}/></button>
+      </div>
       </div>
         
         
