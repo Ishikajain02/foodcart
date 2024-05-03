175f550d164bee74ab46a2a0190f48cc38a224fa
Ishikajain02 <ishijain02@gmail.com>
2024-05-03 21:04:50 +0530

    changes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCircleArrowLeft,faCircleArrowRight} from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect, useState } from 'react'
 import CarouselItem from './CarouselItem'
 import {url} from "../utils/constant";
@@ -16,7 +18,13 @@ const Carousel = () => {
   console.log(carousel);
   return (
     <div className=' flex  flex-col h-[15rem] w-[85rem]'>
+      <div className='flex flex-row justify-between'>
         <h4 className='font-bold text-2xl'>What's on your mind ?</h4>
+        <div className='flex '>
+        <div className='mr-3'><FontAwesomeIcon icon={faCircleArrowLeft} style={{color: "#e2e2e7",height:"40"}} /></div>
+        <div className=''><FontAwesomeIcon icon={faCircleArrowRight} style={{height:"40",color: "#e2e2e7"}} /></div>
+        </div>
+        </div>
         {/*console.log(carousel[0].imageId)*/}
         <div className='flex flex-row  flex-nowrap overflow-hidden overflow-x-scroll h-52 w-[85rem]'>
         {carousel.map((item)=>{
